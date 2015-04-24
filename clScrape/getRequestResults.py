0f4be1de5b7e0398201f64d797e28822cf3dc1a5
landwehrrl <landwehrrl@yahoo.com>
2015-04-24 04:02:31 +0200

    added multi-threading

--- clScrape/getRequestResults.py
+++ clScrape/getRequestResults.py
@@ -1,9 +1,31 @@
 from urllib2 import Request, urlopen, URLError
+from threading import Thread
+from Queue import Queue
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s] (%(threadName)-10s_ %(message)s',
+                    )
+pageQueue = Queue()
+pageObjs = []
 
 def getRequestResults(sites):
-  pageObjs = []
-  for site in sites:
-    print "Checking: " + str(site['string'])
+  sortedObjs = []
+  for i in range(len(sites)):
+    t = Thread(target=gotoSite, args=(pageQueue,))
+    t.setDaemon = True
+    t.start()
+  for s in sites:
+    pageQueue.put(s)
+  
+  pageQueue.join()
+  #sort by location
+  sortedObjs = sorted(pageObjs, key=lambda k: k['linkRoot'])
+  return sortedObjs
+
+def gotoSite(q):
+  while True:
+    site = q.get()
+    logging.debug("Loading: %s" % site['string'] )
     s = site['string']
     index = s.index('.')
     name = s[7:index]
@@ -14,4 +36,6 @@ def getRequestResults(sites):
       pageObjs.append({"linkRoot":"http://" + name + ".craigslist.org","page":cl,"querySet":site['querySet']})
     except:
       print 'nothing to see here'
-  return pageObjs    
+    logging.debug("Processed: %s" % site['string'] )
+    q.task_done()
+
